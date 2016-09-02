666e05d16c4f047ff04a496a2e528371d3d7eaa9
Juliette3529 <Juliette@saint-jalme.fr>
2016-09-02 15:05:32 +0200

    creation template accueil

---
+++ page-templates/acceuil.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Full width Page Template
+ *
+ * @package WordPress
+ * @subpackage prestro
+ *
+ */
+get_header();
+?>
+<div class="container">
+</div>
+<?php get_footer(); ?>
