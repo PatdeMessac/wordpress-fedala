1def95d7115307628e6d72bed1a8f7e8ccb43f34
Juliette3529 <Juliette@saint-jalme.fr>
2016-09-02 15:08:18 +0200

    creation page template

--- page-templates/acceuil.php
+++ page-templates/acceuil.php
@@ -9,5 +9,12 @@
 get_header();
 ?>
 <div class="container">
+<div class="presentation col-md-12"></div>
+</div>
+<div class="paralax col-md-12"></div>
+</div>
+<div class="services col-md-12"></div>
+</div>
+<div class="temoignages col-md-12"></div>
 </div>
 <?php get_footer(); ?>
