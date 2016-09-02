3810f64f3c99ecc704b66ebde723ed05857ca94e
Juliette3529 <Juliette@saint-jalme.fr>
2016-09-02 16:03:46 +0200

    place des images dans service Accueil

--- page-templates/acceuil.php
+++ page-templates/acceuil.php
@@ -8,17 +8,25 @@
  */
 get_header();
 ?>
-<div class="container-fluid">
-<div class="presentation col-md-12">
+    <div class="container-fluid">
+        <div class="presentation col-md-12">
 
-</div>
+        </div>
 
-<div class="paralax col-md-12"></div>
-
-
-<div class="services col-md-12"></div>
+        <div class="paralax col-md-12"></div>
 
 
+        <div class="services col-md-12">
+            <div class="img-responsive col-md-3">
+            <h3>Traiteur</h3>
+            </div>
+            <div class="img-responsive col-md-offset-1 col-md-3">
+            <h3>Décoration</h3>
+            </div>
+            <div class="img-responsive col-md-offset-1 col-md-3">
+            <h3>Location</h3>
+            </div>
+        </div>
 <div class="temoignages col-md-12"></div>
 
 </div>
