daa72f7ed07b6ae6dd70f2a7206eae095f4130a7
Juliette3529 <Juliette@saint-jalme.fr>
2016-09-02 16:34:08 +0200

    changement services

--- page-templates/acceuil.php
+++ page-templates/acceuil.php
@@ -23,17 +23,30 @@ get_header();
         <div class="paralax col-md-12"></div>
 
 
-        <div class="services col-md-12">
+        <div class="row services">
+           <div class="col-md-12 ">
             <div class="img-responsive col-md-3">
+            <h3>Super image</h3>
+            </div>
+            <div class="img-responsive col-md-offset-1 col-md-3">
+            <h3>modifiable</h3>
+            </div>
+            <div class="img-responsive col-md-offset-1 col-md-3">
+            <h3>par client</h3>
+            </div>
+        </div>
+        <div class="col-md-12 ">
+            <div class=" col-md-3">
             <h3>Traiteur</h3>
             </div>
-            <div class="img-responsive col-md-offset-1 col-md-3">
+            <div class="col-md-offset-1 col-md-3">
             <h3>Décoration</h3>
             </div>
-            <div class="img-responsive col-md-offset-1 col-md-3">
+            <div class="col-md-offset-1 col-md-3">
             <h3>Location</h3>
             </div>
         </div>
+</div>
 <div class="temoignages col-md-12"></div>
 
 </div>
