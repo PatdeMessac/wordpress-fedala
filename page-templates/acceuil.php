ea58e04cb3d13127f29b6536e9b05b810e4d1f84
Juliette3529 <Juliette@saint-jalme.fr>
2016-09-02 15:18:24 +0200

    container accueil

--- page-templates/acceuil.php
+++ page-templates/acceuil.php
@@ -10,11 +10,15 @@ get_header();
 ?>
 <div class="container">
 <div class="presentation col-md-12"></div>
-</div>
+
 <div class="paralax col-md-12"></div>
-</div>
+
+
 <div class="services col-md-12"></div>
-</div>
+
+
 <div class="temoignages col-md-12"></div>
+
 </div>
+
 <?php get_footer(); ?>
