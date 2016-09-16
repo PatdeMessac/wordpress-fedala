4efe5c8efd3dfc9a9a412ca7cad24e9d4f69e8bd
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-09-16 16:39:23 +0200

    import new catalog plug-in

---
+++ wp-content/plugins/ecommerce-product-catalog/modules/attributes/index.php
@@ -0,0 +1,25 @@
+<?php
+
+if ( !defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+/**
+ * Manages product includes folder
+ *
+ * Here all plugin includes folder is defined and managed.
+ *
+ * @version		1.0.0
+ * @package		ecommerce-product-catalog/includes
+ * @author 		Norbert Dreszer
+ */
+require_once(AL_BASE_PATH . '/modules/attributes/attributes-conditionals.php');
+
+require_once(AL_BASE_PATH . '/modules/attributes/attributes-settings.php');
+
+require_once(AL_BASE_PATH . '/modules/attributes/attributes-meta.php');
+
+require_once(AL_BASE_PATH . '/modules/attributes/attributes-functions.php');
+require_once(AL_BASE_PATH . '/modules/attributes/product-attributes.php');
+require_once(AL_BASE_PATH . '/modules/attributes/attributes-shortcodes.php');
+
+
