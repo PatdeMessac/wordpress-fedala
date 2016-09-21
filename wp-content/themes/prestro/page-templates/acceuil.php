d9b59291cbd6717d40450c68d106eeec0464d72c
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-09-21 14:13:42 +0200

    accueil.php - customizable introduction text

--- wp-content/themes/prestro/page-templates/acceuil.php
+++ wp-content/themes/prestro/page-templates/acceuil.php
@@ -6,73 +6,65 @@
  * @subpackage prestro
  *
  */
+
+the_post();
+$description = get_the_content();
+
 get_header();
+
 ?>
-<div id="box_service container-fluid">
+
+<div class="container-fluid">
+	
+	<!-- bandeau 2 images - début -->
 	<div class="row">
+		
+		<!-- image "Décoration" -->
 		<?php $post = get_page_by_title('Décoration'); ?>
 		<div class="left col-lg-5 col-lg-offset-1">
 			<a href="<?php the_permalink(); ?>">
 				<img class="img-accueil" src="<?php the_post_thumbnail_url(array(250,250)); ?>" />
 			</a>
 		</div>
+		
+		<!-- image "Traiteur" -->
 		<?php $post = get_page_by_title('Traiteur'); ?>
 		<div class="right col-lg-5">
 			<a href="<?php the_permalink(); ?>">
 				<img class="img-accueil" src="<?php the_post_thumbnail_url(array(250,250)); ?>" />
 			</a>
 		</div>
-	</div>
-
-    <!--<div class="primary">
-    	<?php query_posts('category_name=image_gauche'); ?>
-           	<?php if(have_posts()) : ?>
-               	<?php while(have_posts()) : the_post();?>
-            <div class="parallelogram standar">
-                <img class="img-accueil" src="<?php the_post_thumbnail_url(array(250,250)); ?>" />
-            </div>
-        		<?php endwhile; ?>
-           <?php endif; ?>
-            <div class="parallelogram small"></div>
-            <?php query_posts('category_name=image_droite'); ?>
-           		<?php if(have_posts()) : ?>
-               		<?php while(have_posts()) : the_post();?>
-            <div class="parallelogram standar">
-                <img class="img-accueil" src="<?php the_post_thumbnail_url(array(250,250)); ?>" />
-            </div>
-            	<?php endwhile; ?>
-           <?php endif; ?>
-    </div>-->
-</div>
-<div class="parallax2"></div>
-<div class="container-fluid">
-	<div class="row">
 		
-		<!-- présentation - début -->
+	</div>
+	<!-- bandeau 2 images - fin -->
+	
+	<div class="parallax2"></div>
+	
+	<!-- présentation - début -->
+	<div class="row">
 		<div class="presentation col-md-12">
 			<div class="row">
 				<div class="col-md-12 text-center">
 				 <h2 class="fedala_title">Fedala</h2>
 				</div>
 				<div class="col-md-offset-3 col-md-6 text-center">
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat temporibus sed architecto ut quasi laboriosam quam provident fuga voluptate aspernatur nisi maiores earum magnam, quidem ipsam omnis necessitatibus molestiae laborum! </br>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat temporibus sed architecto ut quasi laboriosam quam provident fuga voluptate aspernatur nisi maiores earum magnam, quidem ipsam omnis necessitatibus molestiae laborum! </p>
-
+					<p><?php echo $description; ?></p>
 				</div>
 			</div>
 		</div>
-		<!-- présentation - fin -->
-
-		<!-- témoignages - début -->
+	</div>
+	<!-- présentation - fin -->
+	
+	<!-- témoignages - début -->
+	<div class="row">
 		<div class="temoignages col-md-12">
 			<div class="parallax3">
 				<?php wp_text_slider(); ?>
 			</div>
 		</div>
-		<!-- services - début -->
-		
 	</div>
+	<!-- témoignages - fin -->
+	
 </div>
 
 <?php get_footer(); ?>
