ed29b66457a13f752ae49e736480a2dcfd658c0e
RonaldM- <ronald.marcel.mr@gmail.com>
2016-09-22 15:19:06 +0200

    C'est parti mon kiki!

--- wp-content/themes/prestro/page-templates/acceuil.php
+++ wp-content/themes/prestro/page-templates/acceuil.php
@@ -6,107 +6,129 @@
  * @subpackage prestro
  *
  */
-get_header();
-?>
-<div id="box_service container-fluid">
-   <div class="wrapper_service row">
-    	<?php query_posts('category_name=image_gauche'); ?>
-           	<?php if(have_posts()) : ?>
-               	<?php while(have_posts()) : the_post();?>
-        <div class="left col-lg-5 col-lg-offset-1">
-           <div class="bloc_ensemble">
-            <div class="block_img_service col-lg-12 row">
-            <img class="img-accueil" src="<?php the_post_thumbnail_url(array(250,250)); ?>" />
-            <div class="block_service col-lg-12 text-center">
-                <div class="box_title col-lg-12">
-                    <?php the_title('<h3  class="title_service col-lg-12">','</h3>'); ?>
-                </div>
-                <span class="separated"></span>
-                <div class="box_extract col-lg-12">
-                    <?php the_content('<p  class="content_service col-lg-12">','</p>'); ?>
-                </div>
-            </div>
-          </div>
-        </div>
-            <?php endwhile; ?>
-       <?php endif; ?>
-       </div>
-        <!--<div class="parallelogram small"></div>-->
-        <?php query_posts('category_name=image_droite'); ?>
-            <?php if(have_posts()) : ?>
-                <?php while(have_posts()) : the_post();?>
-        <div class="right col-lg-5">
-              <div class="bloc_ensemble">
-                <div class="block_img_service col-lg-12 row">
-                <img class="img-accueil" src="<?php the_post_thumbnail_url(array(250,250)); ?>" />
-                <div class="block_service col-lg-12 text-center">
-                    <div class="box_title col-lg-12">
-                        <?php the_title('<h3  class="title_service col-lg-12">','</h3>'); ?>
-                    </div>
-                    <span class="separated"></span>
-                    <div class="box_extract col-lg-12">
-                        <?php the_content('<p  class="content_service col-lg-12">','</p>'); ?>
-                    </div>
-                </div>
-              </div>
-            </div>
-           <!--<div class="bloc_ensemble">
-            <div class="block_img_service col-lg-12 row">
-                <img class="img-accueil" src="<?php the_post_thumbnail_url(array(250,250)); ?>" />
-        </div>-->
-            <?php endwhile; ?>
-       <?php endif; ?>
-</div>
 
-    <!--<div class="primary">
-    	<?php query_posts('category_name=image_gauche'); ?>
-           	<?php if(have_posts()) : ?>
-               	<?php while(have_posts()) : the_post();?>
-            <div class="parallelogram standar">
-                <img class="img-accueil" src="<?php the_post_thumbnail_url(array(250,250)); ?>" />
-            </div>
-        		<?php endwhile; ?>
-           <?php endif; ?>
-            <div class="parallelogram small"></div>
-            <?php query_posts('category_name=image_droite'); ?>
-           		<?php if(have_posts()) : ?>
-               		<?php while(have_posts()) : the_post();?>
-            <div class="parallelogram standar">
-                <img class="img-accueil" src="<?php the_post_thumbnail_url(array(250,250)); ?>" />
-            </div>
-            	<?php endwhile; ?>
-           <?php endif; ?>
-    </div>-->
-</div>
-<div class="parallax2"></div>
+the_post();
+$description = get_the_content();
+
+get_header();
+
+?>
+
 <div class="container-fluid">
+
+	<!--Antoine bandeau 2 images - début -->
+	<!--<div class="row bandeau-accueil">
+
+		<!- image "Décoration" -->
+		<!--<?php $post = get_page_by_title('Décoration'); ?>
+		<div class="left col-xs-5 col-xs-offset-1">
+			<a href="<?php the_permalink(); ?>">
+				<img class="img-accueil" src="<?php the_post_thumbnail_url(array(250,250)); ?>" />
+				<div class="img-foreground">
+					<p><?php the_title(); ?></p>
+				</div>
+			</a>
+		</div>-->
+
+		<!-- image "Traiteur" -->
+	<!--	<?php $post = get_page_by_title('Traiteur'); ?>
+		<div class="right col-xs-5">
+			<a href="<?php the_permalink(); ?>">
+				<img class="img-accueil" src="<?php the_post_thumbnail_url(array(250,250)); ?>" />
+				<div class="img-foreground">
+					<p><?php the_title(); ?></p>
+				</div>
+			</a>
+		</div>
+
+	</div>-->
+	<!-- bandeau 2 images - fin -->
+	<!-- ronald bandeau 2 images -->
+	<div id="box_service container-fluid">
+	   <div class="wrapper_service row">
+
+	     	<!-- image "Décoration" -->
+
+	    <?php $post = get_page_by_title('Décoration'); ?>
+	    <div class="left col-lg-5 col-lg-offset-1">
+	      <a href="<?php the_permalink(); ?>">
+	        <div class="bloc_ensemble">
+	          <div class="block_img_service col-lg-12 row">
+	            <img class="img-accueil" src="<?php the_post_thumbnail_url(array(250,250)); ?>" />
+	            <div class="block_service col-lg-12 text-center">
+	              <div class="box_title col-lg-12">
+	                  <?php the_title('<h3  class="title_service col-lg-12">','</h3>'); ?>
+	              </div>
+	              <span class="separated"></span>
+	              <div class="box_extract col-lg-12">
+	                  <?php the_content('<p  class="content_service col-lg-12">','</p>'); ?>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+	      </a>
+	    </div>
+
+	      <!-- image "Traiteur" -->
+
+	    <?php $post = get_page_by_title('Traiteur'); ?>
+	    <div class="right col-lg-5">
+	      <a href="<?php the_permalink(); ?>">
+	        <div class="bloc_ensemble">
+	          <div class="block_img_service col-lg-12 row">
+	              <img class="img-accueil" src="<?php the_post_thumbnail_url(array(250,250)); ?>" />
+	              <div class="block_service col-lg-12 text-center">
+	                  <div class="box_title col-lg-12">
+	                      <?php the_title('<h3  class="title_service col-lg-12">','</h3>'); ?>
+	                  </div>
+	                  <span class="separated"></span>
+	                  <div class="box_extract col-lg-12">
+	                      <?php the_content('<p  class="content_service col-lg-12">','</p>'); ?>
+	                  </div>
+	              </div>
+	            </div>
+	          </div>
+	        </a>
+	      </div>
+			</div>
+		</div>
+	<!--ronald bandeau 2 images - fin -->
+
+	<div class="parallax2"></div>
+
+	<!-- présentation - début -->
 	<div class="row">
-		
-		<!-- présentation - début -->
 		<div class="presentation col-md-12">
 			<div class="row">
 				<div class="col-md-12 text-center">
 				 <h2 class="fedala_title">Fedala</h2>
 				</div>
 				<div class="col-md-offset-3 col-md-6 text-center">
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat temporibus sed architecto ut quasi laboriosam quam provident fuga voluptate aspernatur nisi maiores earum magnam, quidem ipsam omnis necessitatibus molestiae laborum! </br>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat temporibus sed architecto ut quasi laboriosam quam provident fuga voluptate aspernatur nisi maiores earum magnam, quidem ipsam omnis necessitatibus molestiae laborum! </p>
-
+					<?php
+						foreach ( explode("\n", $description) as $bloc ) {
+							if (preg_match('/[a-z]/i', $bloc)) {
+								echo "<p>";
+								echo $bloc;
+								echo "</p>\n";
+							}
+						}
+					?>
 				</div>
 			</div>
 		</div>
-		<!-- présentation - fin -->
+	</div>
+	<!-- présentation - fin -->
 
-		<!-- témoignages - début -->
+	<!-- témoignages - début -->
+	<div class="row">
 		<div class="temoignages col-md-12">
 			<div class="parallax3">
 				<?php wp_text_slider(); ?>
 			</div>
 		</div>
-		<!-- services - début -->
-		
 	</div>
+	<!-- témoignages - fin -->
+
 </div>
 
 <?php get_footer(); ?>
