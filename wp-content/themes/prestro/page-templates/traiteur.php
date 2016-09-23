ec2a75f3b3f7ff3a6da48716aca81c5599f3e0f1
RonaldM- <ronald.marcel.mr@gmail.com>
2016-09-23 17:10:42 +0200

    Ajoute de parti modal tyjow

--- wp-content/themes/prestro/page-templates/traiteur.php
+++ wp-content/themes/prestro/page-templates/traiteur.php
@@ -6,9 +6,40 @@
 *
 */
 
+$separateur = " - ";
+
 get_header();
 
 ?>
+
+<?php  if (!is_user_logged_in()):?>
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <!-- <div class="modal-header">
+      <span class="close">times</span>
+      <h2><a href="#">Connectez-vous</a></h2>
+    </div> -->
+    <div class="modal-body">
+    	<span class="close">×</span>
+    <?php query_posts('category_name=modal'); ?>
+    	<?php if ( have_posts()) : ?>
+			<?php while ( have_posts()) : the_post(); ?>
+			        <h3><?php the_title(); ?></h3>
+			        <p><?php the_content(); ?></p>
+			<?php endwhile; ?>
+		<?php endif; ?>
+    	<!-- <h3>Information</h3>
+    	<p>Inscrivez-vous puis connectez-vous pour connaître nos tarifs !</p> -->
+    </div>
+    <!-- <div class="modal-footer">
+      <h3>Modal Footer</h3>
+    </div> -->
+  </div>
+</div>
+<?php endif;?>
 
 <div class="container-traiteur">
 	<div class="row">
@@ -31,14 +62,14 @@ get_header();
 							$liste_de_plats = explode("\n", $post->post_content);
 							foreach ($liste_de_plats as $plat) {
 								if (preg_match('/[a-z]/i', $plat)) {
-									$plat_splitted = explode(" - ", $plat);
+									$plat_splitted = explode($separateur, $plat);
 									$nom = $plat_splitted[0];
 									$prix = $plat_splitted[1];
-									echo "<li>";
+									echo "<li class='liste_prix'>";
 									echo $nom;
 									if (is_user_logged_in()) {
-										echo " - ";
-										echo $prix;
+										echo $separateur;
+										echo "<span class='prix_traiteur'>" . $prix . "</span>";
 									}
 									echo "</li>\n";
 								}
@@ -51,5 +82,32 @@ get_header();
 		</div>
 	</div>
 </div>
+<script>
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+//var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+window.onload = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
 
 <?php get_footer(); ?>
