e683a2493d8d2151f86a84a0f802d16f78dc2812
RonaldM- <ronald.marcel.mr@gmail.com>
2016-09-23 16:00:54 +0200

    Déplacement de la balise img dans foreground  dans page traiteur

--- wp-content/themes/prestro/page-templates/traiteur.php
+++ wp-content/themes/prestro/page-templates/traiteur.php
@@ -6,49 +6,49 @@
 *
 */
 
-$separateur = " - ";
-
 get_header();
 
 ?>
 
 <div class="container-traiteur">
 	<div class="row">
-		<?php
-			$query = new WP_Query();
-			$all_pages = $query->query(array('post_type' => 'page'));
-			$traiteur =  get_page_by_title('Traiteur');
-			$subpages = array_reverse(get_page_children($traiteur->ID, $all_pages));
-			foreach ($subpages as $post):
-		?>
-		<div class="col-lg-3 col-sm-6">
-			<img class="img-responsive" src="<?php the_post_thumbnail_url(); ?>" />
-			<div class="img-foreground">
-				<p><?php the_title(); ?></p>
-			</div>
-			<div class="price-ground style-3">
-				<ul>
-					<?php
-						$liste_de_plats = explode("\n", $post->post_content);
-						foreach ($liste_de_plats as $plat) {
-							if (preg_match('/[a-z]/i', $plat)) {
-								$plat_splitted = explode($separateur, $plat);
-								$nom = $plat_splitted[0];
-								$prix = $plat_splitted[1];
-								echo "<li class='liste_prix'>";
-								echo $nom;
-								if (is_user_logged_in()) {
-									echo $separateur;
-									echo $prix;
+		<div class="wrapper_tarif col-md-10 col-md-offset-1">
+			<?php
+				$query = new WP_Query();
+				$all_pages = $query->query(array('post_type' => 'page'));
+				$traiteur =  get_page_by_title('Traiteur');
+				$subpages = array_reverse(get_page_children($traiteur->ID, $all_pages));
+				foreach ($subpages as $post):
+			?>
+			<div class="col-sm-3">
+				<div class="img-foreground col-lg-12">
+					<img class="img-responsive" src="<?php the_post_thumbnail_url(); ?>" />
+					<p><?php the_title(); ?></p>
+				</div>
+				<div class="price-ground style-3 col-lg-12">
+					<ul>
+						<?php
+							$liste_de_plats = explode("\n", $post->post_content);
+							foreach ($liste_de_plats as $plat) {
+								if (preg_match('/[a-z]/i', $plat)) {
+									$plat_splitted = explode(" - ", $plat);
+									$nom = $plat_splitted[0];
+									$prix = $plat_splitted[1];
+									echo "<li>";
+									echo $nom;
+									if (is_user_logged_in()) {
+										echo " - ";
+										echo $prix;
+									}
+									echo "</li>\n";
 								}
-								echo "</li>\n";
 							}
-						}
-					?>
-				</ul>
+						?>
+					</ul>
+				</div>
 			</div>
+			<?php endforeach; ?>
 		</div>
-		<?php endforeach; ?>
 	</div>
 </div>
 
