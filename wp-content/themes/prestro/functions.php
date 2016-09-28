41a0adbbace8c5031b57e1ef8adf3d12bc3612b9
foch01 <tsanceo@yahoo.com>
2016-09-28 14:41:45 +0200

    add script.js + delete function js

--- wp-content/themes/prestro/functions.php
+++ wp-content/themes/prestro/functions.php
@@ -165,13 +165,10 @@ function prestro_enq_styles() {
 add_action('wp_enqueue_scripts', 'prestro_enq_styles');
 
 function prestro_enq_scripts(){
-    wp_enqueue_script('ninja-slider', get_template_directory_uri() . '/inc/js/ninja-slider.js');
-    wp_enqueue_script('thumbnail-slider', get_template_directory_uri() . '/inc/js/thumbnail-slider.js');
+     wp_enqueue_script('main-js', get_template_directory_uri() . '/inc/js/script.js', array('jquery'), '1.0.0', true);
     wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/inc/js/bootstrap.min.js', array('jquery'), '3.3.4', true);
     wp_enqueue_script('ui-js', get_template_directory_uri() . '/inc/js/jquery-ui.min.js', array('jquery'), '1.11.4', true);
-    wp_enqueue_script('carojs', get_template_directory_uri() . '/inc/js/jquery.carouFredSel-6.2.1-packed.js', array('jquery'), '6.2.1', true);
     wp_enqueue_script('wowjs', get_template_directory_uri() . '/inc/js/wow.min.js', array('jquery'), '1.0.0', true);
-    wp_enqueue_script('nivo-js', get_template_directory_uri() . '/inc/js/jquery.nivo.slider.js', array('jquery'), '2.0.8', true);
     wp_enqueue_script('meanmenu', get_template_directory_uri() . '/inc/js/jquery.meanmenu.min.js', array('jquery'), '3.2.0', true);
     wp_enqueue_script('fancybox', get_template_directory_uri() . '/inc/js/jquery.fancybox.pack.js', array('jquery'), '2.1.5', true);
     wp_enqueue_script('para-plug', get_template_directory_uri() . '/inc/js/parallax.min.js', array('jquery'), '1.0.0', true);
